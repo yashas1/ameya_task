feeed5ae4326df3d204f112cea54eace4ca9e4fd
yashas Bhadregowda <yashu.gsb@gmail.com>
2023-01-24 21:59:43 -0500

    updating table

--- src/components/OrganizationStructure.js
+++ src/components/OrganizationStructure.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+import { Typography } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import ComponentHeader from "./ComponentHeader";
-function createData(name, calories, fat) {
-  return { name, calories, fat };
+function createData(Groups, Subgroups, Clinics) {
+  return { Groups, Subgroups, Clinics };
 }
 
 const rows = [
@@ -24,35 +24,75 @@ const OrganizationStructure = () => {
     <>
       <ComponentHeader value={"Organization Structure"} />
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table
+          sx={{
+            minWidth: 650,
+            [`& .${tableCellClasses.root}`]: {
+              borderBottom: "none",
+            },
+          }}
+          aria-label="simple table"
+        >
           <TableHead>
             <TableRow>
-              <TableCell>
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
                 <ComponentHeader value={"Groups"} />
               </TableCell>
 
-              <TableCell>
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
                 <ComponentHeader value={"Subgroups"} />
               </TableCell>
-              <TableCell>
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
                 <ComponentHeader value={"Clinics / Cohorts"} />
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
+              <TableRow key={row.Groups}>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{
+                    border: 2,
+                    borderBottom: "none",
+                    borderColor: "#1F1769",
+                  }}
+                >
+                  <Typography
+                    edge="start"
+                    component="div"
+                    sx={{
+                      m: 2,
+                      fontSize: 18,
+                      color: "#1F1769",
+                      display: "flex",
+                    }}
+                  >
+                    {row.Groups}
+                  </Typography>
                 </TableCell>
-                <TableCell align="left">{row.calories}</TableCell>
-                <TableCell align="right"></TableCell>
+                <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
+                  {row.Subgroups}
+                </TableCell>
+                <TableCell></TableCell>
               </TableRow>
             ))}
           </TableBody>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
+                <ComponentHeader value={"Groups"} />
+              </TableCell>
+
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
+                <ComponentHeader value={"Subgroups"} />
+              </TableCell>
+              <TableCell sx={{ border: 2, borderColor: "#1F1769" }}>
+                <ComponentHeader value={"Clinics / Cohorts"} />
+              </TableCell>
+            </TableRow>
+          </TableHead>
         </Table>
       </TableContainer>
     </>
